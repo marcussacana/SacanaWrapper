fffaf92d3814e81aff42e549c1023888cece50bb
marcussacana <ddtank.marcus@gmail.com>
2019-09-11 18:50:08 -0300

    Add Non Compression for PSB files

---
+++ Updater/KrKrSceneManagerNoMDF.cs
@@ -0,0 +1,21 @@
+﻿#IMPORT %CD%\Plugins\KrKrSceneManager.dll
+using KrKrSceneManager;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public class KrKrSceneManagerNoMDF {
+    PSBAnalyzer Editor;
+    public KrKrSceneManagerNoMDF(byte[] Script) {
+        Editor = new PSBAnalyzer(Script);
+        Editor.CompressPackget = false;
+    }
+
+    public string[] Import() {
+        return Editor.Import();
+    }
+
+    public byte[] Export(string[] Strings) {
+        return Editor.Export(Strings);
+     }
+}
