fef73bd46605ba8ec9a1a348ae95b61dbd776aa6
Marcus André <ddtank.marcus@gmail.com>
2021-04-28 17:33:44 -0300

    Update GBNManager

---
+++ Updater/GBNManagerSJIS.cs
@@ -0,0 +1,21 @@
+﻿#IMPORT %CD%\Plugins\GBNManager.dll
+#IMPORT System.Core.dll
+using GBNManager;
+using System;
+using System.Text;
+
+public class GBNSJIS {
+    GBN Editor;
+	
+    public GBNSJIS(byte[] Script) {
+        Editor = new GBN(Script, Encoding.GetEncoding(932));
+    }
+
+    public string[] Import() {
+        return Editor.Import();
+    }
+
+    public byte[] Export(string[] Strings) {
+        return Editor.Export(Strings);
+	}
+}
